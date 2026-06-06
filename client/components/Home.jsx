00a1f2819227e17983dc6a018df0ca7992015069
Sean Villamonte <seanpajarillo@gmail.com>
2026-06-06 08:15:50 -0400

    06/06 commit

---
+++ client/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+
+  return (
+  <div>
+    <h2 class="fade-in-text">Transforming Ideas into Reality.</h2>
+    <h2 class="fade-in-text">Learn more <a href="/about">about me</a>.</h2>
+  </div>
+  );
+}
---
+++ client/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Layout(){
+     return (
+    // This component is used to display the header and navigation links on all pages
+    <>
+      <img src="/public/favicon.svg" alt="Logo" style={{ width: "100px", height: "100px" }} />
+      <h1>Sean Villamonte</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/project">Project</Link> | <Link to="/services">Services</Link> |{" "}
+        <Link to="/references">References</Link> | <Link to="/contact">Contact</Link>
+      </nav>
+      <br />
+      <hr />
+    </>
+  );
+}
