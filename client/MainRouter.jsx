fe8d2c69effb3bdd859de830513cc5f786a9b18b
Sean Villamonte <seanpajarillo@gmail.com>
2026-06-06 09:00:29 -0400

    Update MainRouter.jsx

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { useHistory } from 'react-router-dom'
 import Home from './components/Home'
 import About from './src/about'
 import Contact from './src/contact'
