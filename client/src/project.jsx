c400267ae50684de3e1b6424e648c9ab7308957c
Sean Villamonte <seanpajarillo@gmail.com>
2026-06-06 08:52:20 -0400

    Update project.jsx

--- client/src/project.jsx
+++ client/src/project.jsx
@@ -4,21 +4,21 @@ export default function Project() {
         <h1>Projects</h1>
      <div class="flexbox">
         <div class="card">
-            <img src="../public/library.png" alt="Project 1" style={{ width: "100%" }} />
+            <img src="./assets/library.png" alt="Project 1" style={{ width: "100%" }} />
             <div class="container">
                 <h3>Online Library Management System</h3>
                 <p>I developed a web-based library management system that allows users to log available, borrowed, and overdue books.</p>
             </div>
         </div>
         <div class="card">
-            <img src="../public/todo.png" alt="Project 2" style={{ width: "100%" }} />
+            <img src="./assets/todo.png" alt="Project 2" style={{ width: "100%" }} />
             <div class="container">
                 <h3>Task Board</h3>
                 <p>I developed a web-based task board system that allows users to create tasks and update their status.</p>
             </div>
         </div>
         <div class="card">
-            <img src="../public/shopping.png" alt="Project 3" style={{ width: "100%" }} />
+            <img src="./assets/shopping.png" alt="Project 3" style={{ width: "100%" }} />
             <div class="container">
                 <h3>Online Shopping List</h3>
                 <p>I developed a web-based shopping list application that allows users to create and manage their shopping lists.</p>
