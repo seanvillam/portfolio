2bf76ec675ffc99beb2d2ff9bcb6d1d572b7da03
Sean Villamonte <seanpajarillo@gmail.com>
2026-06-06 08:27:23 -0400

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
   <div>
     <h2 class="fade-in-text">Transforming Ideas into Reality.</h2>
-    <h2 class="fade-in-text">Learn more <a href="/about">about me</a>.</h2>
+    <h2 class="fade-in-text">Learn more <Link to="/about">about me</Link>.</h2>
   </div>
   );
 }
